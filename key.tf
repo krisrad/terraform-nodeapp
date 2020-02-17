9be40427efe566780b3e203c99f67508204494b8
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-02-17 13:30:42 +0530

    changed keypair name

--- key.tf
+++ key.tf
@@ -1,11 +1,11 @@
 resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
+  key_name   = "mynodekeypair"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
   lifecycle {
     ignore_changes = [public_key]
   }
   tags = {
-    Name = "mykeypair"
+    Name = "mynodekeypair"
     "Project Name" = var.AWS_PROJECT_NAME
   }
 }
