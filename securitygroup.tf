45987169be155919a384e1efc21dd0c32cf09fb5
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-02-17 12:51:37 +0530

    fixing ip add in sec group

--- securitygroup.tf
+++ securitygroup.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "app-securitygroup" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [local.workstation-external-cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
