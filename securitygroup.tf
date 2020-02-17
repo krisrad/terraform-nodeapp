ec24af3cac4f9b215671b5845a6ab33e1b9f19bd
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2020-02-17 12:58:10 +0530

    add external ip

--- securitygroup.tf
+++ securitygroup.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "app-securitygroup" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [local.workstation-external-cidr]
   }
   ingress {
     from_port   = 80
